a9872a2bb76a79578e1036cea0c10d44ce1e80d5
Devin Raleigh <devinkr@gmail.com>
2022-06-27 15:04:19 -0600

    Add colors

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
+var colors = require('colors');
 
 fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
 	if (err) {
-		console.log(err);
+		console.log(err.red);
 	} else {
-		console.log(data);
+		const resume = JSON.parse(data);
+		for (const prop in resume) {
+			if (Array.isArray(resume[prop])) {
+				console.log(`${prop.underline.green}:`);
+				resume[prop].forEach((element) => {
+					console.log(`\t${element.brightYellow}`);
+				});
+			} else {
+				console.log(`${prop.underline.green}: ${resume[prop].cyan}`);
+			}
+		}
 		return data;
 	}
 });
